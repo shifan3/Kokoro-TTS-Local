ef6b56253c93e17c8259a7fe64ec820e82977b7c
shi.fan <shifan3rd@gmail.com>
2025-07-28 07:29:34 +0000

    weqwe

--- start_server.sh
+++ start_server.sh
@@ -4,5 +4,5 @@ PWD=$(pwd)
 docker kill kokoro-server || true
 docker rm kokoro-server || true
 docker run --rm -it -d --name kokoro-server --cap-add SYS_ADMIN --runtime nvidia --device /dev/fuse \
-    --security-opt apparmor=unconfined -v $PWD:/root/kokoro -v /mnt/:/mnt/ -e LANG=C.UTF-8 -p 5006:13256 \
+    --security-opt apparmor=unconfined -v $PWD:/root/kokoro -v /mnt/:/mnt/ -e LANG=C.UTF-8 -p 5006:5072 \
     kokoro:1.0.0 bash -c "cd /root/kokoro && NUM_WORKER=$NUM_WORKER bash do_start_server.sh && sleep infinity"
