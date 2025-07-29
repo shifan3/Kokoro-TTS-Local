d6e9d6cad6a70cb88b62a5caf8fe138adb75b8d5
shi.fan <shifan3rd@gmail.com>
2025-07-29 05:40:51 +0000

    wqe

--- start_server.sh
+++ start_server.sh
@@ -10,7 +10,7 @@ NOCOLOR='\033[0m'
 docker kill kokoro-server || true
 docker rm kokoro-server || true
 docker run --rm -it -d --name kokoro-server --cap-add SYS_ADMIN --runtime nvidia --device /dev/fuse \
-    --security-opt apparmor=unconfined -v $PWD:/root/kokoro -e LANG=C.UTF-8 -p 5006:5072 \
+    --security-opt apparmor=unconfined -v $PWD:/root/kokoro -e LANG=C.UTF-8 -p 5072:13256 \
     kokoro:$VERSION bash -c "cd /root/kokoro && NUM_WORKER=$NUM_WORKER bash do_start_server.sh && sleep infinity"
 
 
